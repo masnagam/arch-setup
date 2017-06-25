ab9a3a0e9019a81a99d328a77edad239395daf91
Masayuki Nagamachi <masayuki.nagamachi@gmail.com>
2017-06-25 12:28:04 +0900

    Add git.sh

---
+++ git.sh
@@ -0,0 +1,11 @@
+# This file is distributed under the MIT license.
+# See LICENSE file in the project root for details.
+
+# Keep this script idempotent.
+
+git config --global fetch.prune true
+git config --global merge.ff false
+git config --global pull.ff only
+git config --global pull.rebase preserve
+git config --global user.name "Masayuki Nagamachi"
+git config --global user.email masayuki.nagamachi@gmail.com
