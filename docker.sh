df4fba6b2c54db4cf76424bdcdbdd7f1e9d4566d
Masayuki Nagamachi <masayuki.nagamachi@gmail.com>
2017-07-16 02:38:40 +0900

    Add docker.sh

---
+++ docker.sh
@@ -0,0 +1,17 @@
+# This file is distributed under the MIT license.
+# See LICENSE file in the project root for details.
+
+# Keep this script idempotent.
+
+if ! which docker >/dev/null 2>&1; then
+    pacaur -S docker
+fi
+
+if ! which docker-compose >/dev/null 2>&1; then
+    pacaur -S docker-compose
+fi
+
+# https://wiki.archlinux.org/index.php/Docker#Installation
+sudo tee /etc/modules-load.d/loop.conf <<<"loop"
+sudo modprobe loop
+systemctl enable docker
