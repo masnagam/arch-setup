c4e1b42599b41ed93faabde70e2158e08773e323
Masayuki Nagamachi <masayuki.nagamachi@gmail.com>
2018-02-25 11:03:09 +0900

    Use ed25519

--- ssh.sh
+++ ssh.sh
@@ -3,13 +3,17 @@
 
 # Keep this script idempotent.
 
+echo Installing keychain...
 if ! which keychain >/dev/null 2>&1; then
     pacaur -S keychain
 fi
 
-mkdir -p $HOME/.bashrc.d
 
 # https://wiki.archlinux.org/index.php/SSH_keys#Keychain
+echo Installing keychain.sh into the .bashrc.d folder...
+mkdir -p $HOME/.bashrc.d
 cat <<'EOF' >$HOME/.bashrc.d/keychain.sh
-eval $(keychain --eval --quiet id_rsa)
+eval $(keychain --eval --quiet id_ed25519)
 EOF
+
+echo Done
