edf47ed68ae12fc997ccaeab121a1d3d4308d90d
masnagam <masnagam@gmail.com>
2020-07-15 12:35:20 +0900

    Update iscsi-boot.sh

--- iscsi-boot.sh
+++ iscsi-boot.sh
@@ -4,9 +4,6 @@ PORTAL="$1"
 TARGET="$2"
 INITIATOR="$3"
 
-echo 'Installing open-iscsi...'
-pacman -Sy open-iscsi
-
 echo 'Installing /usr/lib/initcpio/install/iscsi...'
 cat <<'EOF' >/usr/lib/initcpio/install/iscsi
 build() {
@@ -27,8 +24,8 @@ HELPEOF
 }
 EOF
 
-echo 'Installing /usr/lib/initcpio/hook/iscsi...'
-cat <<EOF
+echo 'Installing /usr/lib/initcpio/hooks/iscsi...'
+cat <<EOF >/usr/lib/initcpio/hooks/iscsi
 run_hook() {
     modprobe iscsi_tcp
     iscsistart -a $PORTAL -t $TARGET -g 1 -i $INITIATOR
@@ -36,7 +33,7 @@ run_hook() {
 EOF
 
 echo 'Updating /etc/mkinitcpio.conf...'
-sed -i 's/^HOOKS=/HOOKS=(base udev autodetect modconf net iscsi block filesystems keyboard fsck)/' /etc/mkinitcpio.conf
+sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf net iscsi block filesystems keyboard fsck)/' /etc/mkinitcpio.conf
 
-echo 'Updating /boot/initramfs-linux*.img...'
-mkinitcpio -p linux
+echo 'Installing packages and updating /boot/initramfs-linux*.img...'
+pacman -Sy --noconfirm open-iscsi mkinitcpio-nfs-utils
