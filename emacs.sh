50dfae144a06660f62025520fbf73acb647ab145
masnagam <masnagam@gmail.com>
2020-10-01 12:54:09 +0900

    Add comments about mozc_emacs_helper

--- emacs.sh
+++ emacs.sh
@@ -25,3 +25,14 @@ EOF
 
 chmod +x $HOME/bin/em
 chmod +x $HOME/bin/kem
+
+# Need to install mozc_emacs_helper from the srouce.
+# See https://github.com/google/mozc/blob/2018-02-26/docs/build_mozc_in_docker.md
+#
+# Run the following commands before building mozc_emacs_helper:
+#
+#   git checkout -f 2018-02-26
+#   git submodule update
+#
+# Use the 2018-02-26 tag so that the mozc_emacs_helper works with the
+# mozc-server installed using pacman, which is based on 2.23.2815.102.
